1b69f6f99129c50d909e335883ae66ea92ba8a03
kanak5522 <kanakchandel552000@gmail.com>
2024-09-06 02:28:29 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,3 +10,12 @@ resource "aws_instance" "kanakchandel" {
     Name = "kanak2"
   }
 }
+resource "aws_instance" "kanakschandel" {
+  ami           = "ami-04e49d62cf88738f1"
+  instance_type = "t2.micro"              
+ 
+
+  tags = {
+    Name = "kanakcc2"
+  }
+}
