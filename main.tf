b176f8d2ee1b0ff0629251794c13696b4754aa01
kanak5522 <kanakchandel552000@gmail.com>
2024-09-06 02:20:31 -0700

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+resource "aws_instance" "kanakchandel" {
+  ami           = "ami-04e49d62cf88738f1"
+  instance_type = "t2.micro"              
+ 
+
+  tags = {
+    Name = "kanak2"
+  }
+}
